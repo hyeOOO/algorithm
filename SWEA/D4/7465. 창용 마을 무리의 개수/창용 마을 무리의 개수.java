fa9dfa8fbb660e56f0b5e573d5c77f01752a238f
hyewon <165285610+hyeOOO@users.noreply.github.com>
2024-09-02 15:57:18 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 148 ms, Memory: 27,432 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -7,15 +7,13 @@ import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Solution
-{
-    static int N, M;
-	static List<List<Integer>> abjList;
-	static boolean[] visited;
+public class Solution {
+	static int N, M;
 	static int result;
-    
-	public static void main(String args[]) throws Exception
-	{
+	static int[] parent;
+	static int[] rank;
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
@@ -25,55 +23,52 @@ class Solution
 			st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
-
-			abjList = new ArrayList<>();
-			visited = new boolean[N + 1];
+			parent = new int[N + 1]; // 각 노드의 부모노드 정보
+			rank = new int[N + 1]; // 트리 높이
 			result = 0;
 
-			for (int i = 0; i < N + 1; i++) {
-				abjList.add(new ArrayList<>());
+			for (int i = 1; i <= N; i++) {
+				parent[i] = i; // 부모노드 초기화
+				rank[i] = 0; // 트리 높이 초기화
 			}
 
-			// 인접리스트 bfs 탐색
-
-			// 인접리스트 초기화
 			for (int i = 0; i < M; i++) {
 				st = new StringTokenizer(br.readLine());
 				int from = Integer.parseInt(st.nextToken());
 				int to = Integer.parseInt(st.nextToken());
 
-				// 무향 그래프 생성
-				abjList.get(from).add(to);
-				abjList.get(to).add(from);
+				union(from, to);
 			}
 
 			for (int i = 1; i <= N; i++) {
-				if (!visited[i]) {
-					bfs(i);
+				if (find(i) == i)
 					result++;
-				}
 			}
-			
+
 			System.out.printf("#%d %d\n", t, result);
 		}
 	}
-    
-    public static void bfs(int start) {
-		Queue<Integer> q = new LinkedList<Integer>();
-		q.offer(start);
-		visited[start] = true;
 
-		while (!q.isEmpty()) {
-			int qSize = q.size();
-			for (int i = 0; i < qSize; i++) {
-				int node = q.poll();
-				for(int j=0; j<abjList.get(node).size(); j++) {
-					int n = abjList.get(node).get(j);
-					if(!visited[n]) {
-						visited[n] = true;
-						q.offer(n);
-					}
-				}
+	public static int find(int n) {
+		if (parent[n] != n) {
+			parent[n] = find(parent[n]);
+		}
+
+		return parent[n];
+	}
+
+	public static void union(int n1, int n2) {
+		int rootN1 = find(n1);
+		int rootN2 = find(n2);
+
+		if (rootN1 != rootN2) {
+			if (rank[rootN1] > rank[rootN2]) {
+				parent[rootN2] = rootN1;
+			} else if (rank[rootN1] < rank[rootN2]) {
+				parent[rootN1] = rootN2;
+			} else {
+				parent[rootN2] = rootN1;
+				rank[rootN1]++;
 			}
 		}
 	}
