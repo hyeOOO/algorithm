5e42deb13ad559c8c36a0f93df4b38b123389fbd
hyewon <165285610+hyeOOO@users.noreply.github.com>
2024-09-06 02:34:30 +0900

    [Silver III] Title: N과 M (4), Time: 128 ms, Memory: 17252 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 비내림차순
+public class Main {
+	static int N, M;
+	static int[] arr;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+
+		for (int i = 1; i <= N; i++) {
+			arr[i - 1] = i;
+		}
+
+		dfs(0, new int[M], 0);
+		
+		System.out.println(sb);
+	}
+
+	public static void dfs(int depth, int[] select, int prev) {
+		if (depth == M) {
+			for (int s : select) {
+				sb.append(s).append(' ');
+			}
+			sb.append('\n');
+
+			return;
+		}
+
+		for (int i = prev; i < N; i++) {
+			select[depth] = arr[i];
+			dfs(depth + 1, select, i);
+		}
+	}
+}
