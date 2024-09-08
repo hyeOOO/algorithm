20ba9ad1ff69d36598a701fe0202e8838ec3650f
hyewon <165285610+hyeOOO@users.noreply.github.com>
2024-09-09 02:27:45 +0900

    [D2] Title: 나무 높이, Time: 143 ms, Memory: 21,620 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -4,37 +4,35 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-/**
- * [swea/14510] ���� ����
- * 
- * @since 2024.08.29
- */
 public class Solution {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		int T = Integer.parseInt(br.readLine());
+
 		for (int t = 1; t <= T; t++) {
 			int N = Integer.parseInt(br.readLine());
+			int[] tree = new int[N];
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int[] trees = new int[N];
+
 			for (int i = 0; i < N; i++) {
-				trees[i] = Integer.parseInt(st.nextToken());
+				tree[i] = Integer.parseInt(st.nextToken());
 			}
 
-			Arrays.sort(trees);
+			Arrays.sort(tree);
 
-			int maxHeight = trees[N - 1];
+			int goal = tree[tree.length - 1];
+
 			long need1 = 0;
 			long need2 = 0;
 
 			for (int i = 0; i < N; i++) {
-				int need = maxHeight - trees[i];
+				int need = goal - tree[i];
 				need2 += need / 2;
 				need1 += need % 2;
 			}
 
-
-			while (need2 > need1 + 1) {
+			while (need2 > need1 + 1) { // 2일만을 기다리는게 아니라 1일을 두번처리 하는 방식을 채택하여 1과 2의 차이 최소화하기
 				need2--;
 				need1 += 2;
 			}
@@ -42,12 +40,12 @@ public class Solution {
 			long result;
 			if (need1 == need2) {
 				result = need2 * 2;
-			} else if (need1 > need2) {
+			} else if (need1 > need2) { // 무조건 1일이 오기를 기다려야 함.
 				result = need1 * 2 - 1;
 			} else {
 				result = need2 * 2;
 			}
-
+			
 			System.out.printf("#%d %d\n", t, result);
 		}
 	}
