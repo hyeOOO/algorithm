64e7b98218e4e48f1c9d32acd5d84c39214b805e
hyewon <165285610+hyeOOO@users.noreply.github.com>
2024-09-09 10:49:40 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 220 ms, Memory: 25,340 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -5,17 +5,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-class Solution
-{
-    static int N, maxCoreCount, minLength;
+public class Solution {
+	static int N, result, connectCores;
 	static int[][] map;
+	static List<int[]> cores;
 	static int[] dy = { -1, 1, 0, 0 };
 	static int[] dx = { 0, 0, -1, 1 };
-	static List<int[]> cores;
-    
-	public static void main(String args[]) throws Exception
-	{
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
 		int T = Integer.parseInt(br.readLine());
 
@@ -23,89 +22,78 @@ class Solution
 			N = Integer.parseInt(br.readLine());
 			map = new int[N][N];
 			cores = new ArrayList<>();
-			maxCoreCount = 0; // 초기값을 0으로 설정해야함
-			minLength = Integer.MAX_VALUE;
+			result = Integer.MAX_VALUE;
+			connectCores = Integer.MIN_VALUE;
 
 			for (int i = 0; i < N; i++) {
-				StringTokenizer st = new StringTokenizer(br.readLine());
+				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < N; j++) {
 					map[i][j] = Integer.parseInt(st.nextToken());
-					if (map[i][j] == 1 && i != 0 && j != 0 && i != N - 1 && j != N - 1) {
+					// 테두리는 자동 연결
+					if (i != 0 && j != 0 && i != N - 1 && j != N - 1 && map[i][j] == 1) {
 						cores.add(new int[] { i, j });
 					}
 				}
 			}
 
-			dfs(0, 0, 0);
+			search(0, 0, 0);
 
-			System.out.println("#" + t + " " + minLength);
+			System.out.printf("#%d %d\n", t, result);
 		}
 	}
-    
-    public static void dfs(int depth, int coreCnt, int cableLen) {
-		// 코어를 모두 처리했을 때
+
+	public static void search(int depth, int coreCnt, int lineCnt) {
 		if (depth == cores.size()) {
-			if (coreCnt > maxCoreCount || (coreCnt == maxCoreCount && cableLen < minLength)) {
-				maxCoreCount = coreCnt; // maxCoreCount를 올바르게 갱신
-				minLength = cableLen; // minLength를 올바르게 갱신
+			if (coreCnt > connectCores) {
+				connectCores = coreCnt;
+				result = lineCnt;
+			} else if (coreCnt == connectCores) {
+				result = Math.min(result, lineCnt);
 			}
 			return;
 		}
 
-		// cores 내 core를 가져와서 dfs하기
-		int[] core = cores.get(depth);
-		int y = core[0];
-		int x = core[1];
+		int[] curCore = cores.get(depth);
 
 		for (int d = 0; d < 4; d++) {
-			int ny = y;
-			int nx = x;
-			int len = 0;
-
-			while (true) {
-				ny += dy[d];
-				nx += dx[d];
-
-				// 배열 끝에 도착하면 빠져나오기
-				if (ny < 0 || nx < 0 || ny >= N || nx >= N)
-					break;
-
-				// 탐색 중 전선이나 코어를 만난 경우 해당 방향으로는 더이상 진행 불가
-				if (map[ny][nx] > 0) {
-					len = 0;
-					break;
-				}
-
-				len++;
-			}
-
-			// 해당 전선이 유효할 때
-			if (len > 0) {
-				ny = y;
-				nx = x;
-
-				// 전선 처리
-				for (int i = 0; i < len; i++) {
-					ny += dy[d];
-					nx += dx[d];
-					map[ny][nx] = 2;
-				}
-
-				// 다음 코어 탐색, 해당 코어는 전선연결 처리 완료, 해당 코어의 전선 길이 가중
-				dfs(depth + 1, coreCnt + 1, cableLen + len);
-
-				// 연결된 전선 해제
-				ny = y;
-				nx = x;
-				for (int i = 0; i < len; i++) {
-					ny += dy[d];
-					nx += dx[d];
-					map[ny][nx] = 0;
-				}
+			int len = isConnect(curCore[0], curCore[1], d);
+			if (len != -1) {
+				// 전선 연결
+				connectWire(curCore[0], curCore[1], d, len, -1);
+				search(depth + 1, coreCnt + 1, lineCnt + len);
+				// 연결 해제
+				connectWire(curCore[0], curCore[1], d, len, 0);
 			}
 		}
 
-		// 해당 코어를 연결하지 않는 경우
-		dfs(depth + 1, coreCnt, cableLen);
+		//해당 코어를 연결하지 않는 경우
+		search(depth + 1, coreCnt, lineCnt);
+	}
+
+	public static int isConnect(int y, int x, int d) {
+		int len = 0;
+		int ny = y, nx = x;
+
+		while (true) {
+			ny += dy[d];
+			nx += dx[d];
+
+			if (ny < 0 || nx < 0 || ny >= N || nx >= N)
+				break;
+			if (map[ny][nx] != 0)
+				return -1;
+			len++;
+		}
+
+		return len;
+	}
+
+	public static void connectWire(int y, int x, int d, int len, int val) {
+		int ny = y, nx = x;
+		for (int i = 0; i < len; i++) {
+			ny += dy[d];
+			nx += dx[d];
+			map[ny][nx] = val;
+		}
 	}
 }
