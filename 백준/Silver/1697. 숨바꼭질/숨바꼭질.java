9bb6134106df0acdd49411a690acc0f042f55356
hyewon <hyewon5342@nate.com>
2024-12-10 22:27:11 +0900

    [Silver I] Title: 숨바꼭질, Time: 152 ms, Memory: 19060 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,58 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, K;
-	static int[] visited = new int[100001];
-
-	public static void bfs(int n) {
-		Queue<Integer> q = new LinkedList<>();
-		q.add(n);
-		visited[n] = 1;
-
-		while (!q.isEmpty()) {
-			int node = q.poll();
-
-			for (int i = 0; i < 3; i++) {
-				int next;
-
-				if (i == 0)
-					next = node - 1;
-				else if (i == 1)
-					next = node + 1;
-				else
-					next = node * 2;
-
-				if (next == K) {
-					System.out.println(visited[node]);
-					return;
-				}
-
-				if (next >= 0 && next < visited.length && visited[next] == 0) {
-					q.add(next);
-					visited[next] = visited[node] + 1;
+	static int K;
+	static boolean[] visited = new boolean[100001];
+	static int[] time = new int[100001];
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		if(N==K) System.out.println(0);
+		else bfs(N);
+	}
+	
+	public static void bfs(int N) {
+		Queue<Integer> q = new LinkedList<Integer>();
+		q.offer(N);
+		
+		while(!q.isEmpty()) {
+			int cur = q.poll();
+			
+			if(cur==K) {
+				System.out.println(time[cur]);
+				return;
+			}
+			int next = 0;
+			// 세가지 경우의 수(x-1, x+1, 2*x)를 다음 스텝으로 구하기
+			for(int i=0; i<3; i++) {
+				if(i==0) next = cur-1;
+				else if(i==1) next = cur+1;
+				else next = cur*2;
+				
+				// 다음 이동이 범위에서 벗어나거나 방문한 위치면 방문x
+				if(next>=0 && next<=100000 && !visited[next]) {
+					q.offer(next);
+					visited[next] = true;
+					time[next] = time[cur]+1;
 				}
 			}
 		}
-	}
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-
-		if (N == K)
-			System.out.println(0);
-		else
-			bfs(N);
-
+		
+		System.out.println(-1);
+		return;
 	}
 }
