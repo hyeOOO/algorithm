030cccf3fda919c6f8ce3d40af42afaabaaba6e5
hyewon <hyewon5342@nate.com>
2024-12-25 13:15:39 +0900

    [Gold V] Title: 수강 과목, Time: 144 ms, Memory: 15312 KB -BaekjoonHub

--- 백준/Gold/17845. 수강 과목/수강 과목.java
+++ 백준/Gold/17845. 수강 과목/수강 과목.java
@@ -12,7 +12,7 @@ public class Main {
 		int K = Integer.parseInt(st.nextToken());
 		int[] I = new int[K+1];
 		int[] T = new int[K+1];
-		int[][] dp = new int[K+1][N+1];
+		int[] dp = new int[N+1];
 		
 		for(int i=1; i<=K; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -21,18 +21,12 @@ public class Main {
 		}
 		
 		for(int i=1; i<=K; i++) {
-			for(int j=1; j<=N; j++) {
-				if(T[i]>j) {
-					dp[i][j] = dp[i-1][j];
-				}
-				
-				else {
-					dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-T[i]]+I[i]);
-				}
+			for(int j=N; j-T[i]>=0; j--) {
+				dp[j] = Math.max(dp[j], dp[j-T[i]]+I[i]);
 			}
 		}
 		
-		System.out.println(dp[K][N]);
+		System.out.println(dp[N]);
 		
 		
 	}
