f4e1a44c602fd665ed4110483cfed17500a350f5
hyewon <hyewon5342@nate.com>
2025-01-26 02:51:21 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 676 ms, Memory: 97516 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -4,35 +4,37 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		char[] chrArr = br.readLine().toCharArray();
-		int[][] alphabetCount = new int[chrArr.length][26];
+        char[] chrArr = br.readLine().toCharArray();
+        int[][] prefixSum = new int[26][chrArr.length]; // 알파벳 26개에 대해 각 인덱스까지의 누적합 저장
 
-		alphabetCount[0][chrArr[0] - 'a']++;
+        // 누적합 배열 초기화
+        prefixSum[chrArr[0] - 'a'][0] = 1;
+        for (int i = 1; i < chrArr.length; i++) {
+            for (int j = 0; j < 26; j++) {
+                prefixSum[j][i] = prefixSum[j][i - 1]; // 이전 값 복사
+            }
+            prefixSum[chrArr[i] - 'a'][i]++; // 현재 문자의 카운트 증가
+        }
 
-		for (int i = 1; i < chrArr.length; i++) {
-			// 이전 값 복사
-			for (int j = 0; j < 26; j++) {
-				alphabetCount[i][j] = alphabetCount[i - 1][j];
-			}
-			alphabetCount[i][chrArr[i] - 'a']++;
-		}
+        int q = Integer.parseInt(br.readLine()); // 질문 개수
+        StringBuilder sb = new StringBuilder();
 
-		int q = Integer.parseInt(br.readLine());
+        while (q-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int alphaIdx = st.nextToken().charAt(0) - 'a';
+            int idx1 = Integer.parseInt(st.nextToken());
+            int idx2 = Integer.parseInt(st.nextToken());
 
-		while (q-- > 0) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int alphaIdx = st.nextToken().charAt(0) - 'a';
-			int idx1 = Integer.parseInt(st.nextToken());
-			int idx2 = Integer.parseInt(st.nextToken());
+            if (idx1 == 0) {
+                sb.append(prefixSum[alphaIdx][idx2]).append("\n");
+            } else {
+                sb.append(prefixSum[alphaIdx][idx2] - prefixSum[alphaIdx][idx1 - 1]).append("\n");
+            }
+        }
 
-			if (idx1 != 0) {
-				System.out.println(alphabetCount[idx2][alphaIdx] - alphabetCount[idx1 - 1][alphaIdx]);
-			}else {
-				System.out.println(alphabetCount[idx2][alphaIdx]);
-			}
-		}
-	}
+        System.out.print(sb);
+    }
 }
