42b0d99be6b4ccfdc04ec9dafe98575274580abf
hyewon <hyewon5342@nate.com>
2025-03-18 22:52:14 +0900

    [Silver I] Title: 숨바꼭질, Time: 136 ms, Memory: 18928 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,54 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	static int K;
-	static boolean[] visited = new boolean[100001];
-	static int[] time = new int[100001];
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		
-		if(N==K) System.out.println(0);
-		else bfs(N);
-	}
-	
-	public static void bfs(int N) {
-		Queue<Integer> q = new LinkedList<Integer>();
-		q.offer(N);
-		
-		while(!q.isEmpty()) {
-			int cur = q.poll();
-			
-			if(cur==K) {
-				System.out.println(time[cur]);
-				return;
-			}
-			int next = 0;
-			// 세가지 경우의 수(x-1, x+1, 2*x)를 다음 스텝으로 구하기
-			for(int i=0; i<3; i++) {
-				if(i==0) next = cur-1;
-				else if(i==1) next = cur+1;
-				else next = cur*2;
-				
-				// 다음 이동이 범위에서 벗어나거나 방문한 위치면 방문x
-				if(next>=0 && next<=100000 && !visited[next]) {
-					q.offer(next);
-					visited[next] = true;
-					time[next] = time[cur]+1;
-				}
-			}
-		}
-		
-		System.out.println(-1);
-		return;
-	}
+    static int N, X;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
+        visited = new boolean[100001]; // 최대 위치는 100,000으로 설정
+
+        bfs();
+    }
+
+    static void bfs() {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[] { N, 0 });
+        visited[N] = true;
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+
+            if (cur[0] == X) {
+                System.out.println(cur[1]);
+                return;
+            }
+
+            // 뒤로 한 칸 이동
+            if (cur[0] - 1 >= 0 && !visited[cur[0] - 1]) {
+                visited[cur[0] - 1] = true;
+                q.offer(new int[] { cur[0] - 1, cur[1] + 1 });
+            }
+
+            // 앞으로 한 칸 이동
+            if (cur[0] + 1 <= 100000 && !visited[cur[0] + 1]) {
+                visited[cur[0] + 1] = true;
+                q.offer(new int[] { cur[0] + 1, cur[1] + 1 });
+            }
+
+            // 순간이동 (2배)
+            if (cur[0] * 2 <= 100000 && !visited[cur[0] * 2]) {
+                visited[cur[0] * 2] = true;
+                q.offer(new int[] { cur[0] * 2, cur[1] + 1 });
+            }
+        }
+    }
 }
