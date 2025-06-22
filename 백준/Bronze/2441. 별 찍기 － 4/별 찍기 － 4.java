fc8028ee6c4ed9b689b8acb8fb0a9257e46cc081
hyewon <hyewon5342@nate.com>
2025-06-22 23:50:23 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 240 ms, Memory: 16088 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int y=0; y<n; y++) {
+			for(int x=0; x<y; x++) {
+				System.out.print(" ");
+			}
+			for(int x=n-y; x>0; x--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
