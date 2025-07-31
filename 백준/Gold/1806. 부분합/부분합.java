d4933fa77cd36d3b0446e48a3c5dd9b160b755a1
hyewon <hyewon5342@nate.com>
2025-07-31 23:25:59 +0900

    [Gold IV] Title: 부분합, Time: 252 ms, Memory: 24316 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -4,40 +4,40 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        int S = Integer.parseInt(st.nextToken());
-        long[] arr = new long[N];
-
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int left = 0;
-        int right = 0;
-        long answer = Long.MAX_VALUE;
-        long total = 0L;
-
-        while(left<=right && left < N){
-            if(total<S && right < N){
-                total += arr[right++];
-            }else if(total>=S){
-                answer = Math.min(answer, right-left);
-                total -= arr[left++];
-            }else {
-                break;
-            }
-        }
-
-        if(answer==Long.MAX_VALUE) {
-            System.out.println(0);
-            return;
-        }
-        System.out.println(answer);
-    }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int S = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		int left = 0;
+		int right = 0;
+		long total = 0;
+		int min = Integer.MAX_VALUE;
+		
+		while(left<=right && left<N) {
+			if(total>=S) {
+				min = Math.min(min, right-left);
+				total -= arr[left++];
+			}else if(total<S && right<N) {
+				total += arr[right++];
+			}else {
+				break;
+			}
+		}
+		
+		if(min == Integer.MAX_VALUE) {
+			System.out.println(0);
+			return;
+		}
+		System.out.println(min);
+		
+	}
 }
