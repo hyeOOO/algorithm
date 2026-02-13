24e65f90fd323e7f554ccca55c8f7f87858800f7
hyewon <hyewon5342@nate.com>
2026-02-13 16:44:00 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,14 +1,12 @@
--- 코드를 입력하세요
-SELECT U.USER_ID,
-        NICKNAME,
-        CONCAT(CITY,' ',STREET_ADDRESS1, ' ', STREET_ADDRESS2) AS '전체주소',
-        CONCAT(SUBSTR(TLNO,1,3),'-',SUBSTR(TLNO,4,4),'-',SUBSTR(TLNO,8)) AS '전화번호'
-FROM USED_GOODS_USER U JOIN (
-                                SELECT USER_ID
-                                FROM USED_GOODS_BOARD B JOIN USED_GOODS_USER U
-                                    ON B.WRITER_ID = U.USER_ID
-                                GROUP BY USER_ID
-                                HAVING COUNT(BOARD_ID)>=3
-                            ) T
-    ON U.USER_ID = T.USER_ID
-ORDER BY U.USER_ID DESC;
+SELECT USER_ID, 
+    NICKNAME,
+    CONCAT(CITY, ' ',CONCAT(STREET_ADDRESS1,' ',STREET_ADDRESS2)) AS '전체주소',
+    CONCAT(LEFT(TLNO,3),'-',SUBSTRING(TLNO,4,4),'-',RIGHT(TLNO,4)) AS '전화번호'
+FROM USED_GOODS_USER
+WHERE USER_ID IN (
+    SELECT WRITER_ID
+    FROM USED_GOODS_BOARD
+    GROUP BY WRITER_ID
+    HAVING COUNT(*)>=3
+)
+ORDER BY USER_ID DESC;
