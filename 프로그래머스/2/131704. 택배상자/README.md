213a4380cec24ea4423a61e96818bbbb1afcd36c
hyewon <hyewon5342@nate.com>
2026-09-05 12:54:06 +0900

    [level 2] Title: 택배상자, Time: 376.04 ms, Memory: 181 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/README.md
+++ 프로그래머스/2/131704. 택배상자/README.md
@@ -1,10 +1,10 @@
 # [level 2] 택배상자 - 131704 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131704?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131704) 
 
 ### 성능 요약
 
-메모리: 147 MB, 시간: 42.90 ms
+메모리: 181 MB, 시간: 376.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 31일 14:48:54
+2026년 09월 05일 12:54:03
 
 ### 문제 설명
 
--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -2,30 +2,39 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] order) {
-        Stack<Integer> sub = new Stack<>();
         int answer = 0;
-        int boxNum = 1; // 현재 컨베이어 벨트에서 나올 박스 번호
+        PriorityQueue<Integer> mainBelt = new PriorityQueue<>();
+        Stack<Integer> subBelt = new Stack<>();
+        for(int o : order){
+            mainBelt.offer(o);
+        }
         
-        for(int want : order) {
-            // 컨베이어 벨트에서 원하는 박스가 나올 때까지 보조 컨베이어에 적재
-            while(boxNum <= order.length && boxNum < want) {
-                sub.push(boxNum);
-                boxNum++;
-            }
-            
-            // 메인 컨베이어에서 바로 실을 수 있는 경우
-            if(boxNum == want) {
+        int targetIdx = 0;
+        while(true){
+            // 모든 상자를 다 꺼낸 경우 중단 
+            if(targetIdx == order.length) break;
+            // mainBelt의 꼭대기와 subBelt 꼭대기 상자가 꺼내야하는 상자와 일치하는지 확인    
+            if(!mainBelt.isEmpty() && mainBelt.peek() == order[targetIdx]){
+                mainBelt.poll();
                 answer++;
-                boxNum++;
-            }
-            // 보조 컨베이어의 맨 위에서 실을 수 있는 경우
-            else if(!sub.isEmpty() && sub.peek() == want) {
-                sub.pop();
+                targetIdx++;
+                continue;
+            }else if(!subBelt.isEmpty() && subBelt.peek() == order[targetIdx]){
+                subBelt.pop();
                 answer++;
-            }
-            // 둘 다 안되면 더 이상 진행 불가
-            else {
-                break;
+                targetIdx++;
+                continue;
+            }else{
+                // 일치하지 않았을 때, stack내 타깃 상자가 있다면 더 이상 못꺼냄
+                if(!subBelt.isEmpty() && subBelt.contains(order[targetIdx])){
+                    break;
+                }
+                // 일치하지 않았을 때, mainBelt내 타깃이 있다면 타깃 상자까지 다 빼야함
+                if(!mainBelt.isEmpty() && mainBelt.contains(order[targetIdx])){
+                    while(mainBelt.peek()!=order[targetIdx]){
+                        subBelt.push(mainBelt.poll());
+                    }
+                }
             }
         }
         
