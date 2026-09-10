86b21e034a0c39410727b3032d32f4be27e6f98a
hyewon <hyewon5342@nate.com>
2026-09-11 03:34:08 +0900

    Update ［3차］ 파일명 정렬.java

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     static class File implements Comparable<File>{
-        int idx;
+        int idx; // 입력 순서
         String head;
         String number;
         String tail;
@@ -19,6 +19,7 @@ class Solution {
         }
 
         public int compareTo(File b){
+            // 대소문자 통일 비교
             String aHead = this.head.toUpperCase();
             String bHead = b.head.toUpperCase();
             
@@ -38,17 +39,18 @@ class Solution {
         String[] answer = new String[files.length];
         List<File> fileList = new ArrayList<>();
         
-        for(int i=0; i<files.length; i++){
+        for(int i = 0; i < files.length; i++){
             String head = "";
             String number = "";
             String tail = "";
             for(char c : files[i].toCharArray()){
-                if(number.length()==0 && (Character.isLetter(c)||c==' '||c=='.'||c=='-')){
+                // number가 비어있고, 문자인 경우 head
+                if(number.length() == 0 && (Character.isLetter(c) || c == ' ' || c == '.' || c == '-')){
                     head += c;
-                }else if(tail.length()==0 && c>='0' && c<='9'){
-                    number+=c;
-                }else{
-                    tail+=c;
+                }else if(tail.length() == 0 && c >= '0' && c <= '9'){ // tail이 비어있고 숫자로 이뤄진 경우 number
+                    number += c;
+                }else{ // 그 외엔 전부 tail
+                    tail += c;
                 }
             }
             fileList.add(new File(i, head, number, tail));
@@ -56,7 +58,7 @@ class Solution {
         
         Collections.sort(fileList);
         
-        for(int i=0; i<files.length; i++){
+        for(int i = 0; i < files.length; i++){
             answer[i] = fileList.get(i).getFile();
         }
         
